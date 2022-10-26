ad5dfdfc274c92d699754a3e36c9dfba09bd8459
EddKentuchi <ediarpoll@gmail.com>
2022-10-26 16:11:57 -0500

    Centrado y ajustado a pantalla completa

--- Registro Pilares.py
+++ Registro Pilares.py
@@ -15,7 +15,7 @@ sTimeStamp = time.strftime('%H%M%S')
 #f.write(sTimeStamp + '\n')
 print (sTimeStamp)
 #Variable de fila de botones
-rowB= 5
+rowB= 4
 
 #Colores
 c1= "#9F2241" #Rojo
@@ -70,58 +70,87 @@ def initPilares(themePilares):
 
 #Creación de Primer ventana
 windowInitP = tk.Tk()
+#obtener tamaño total de la pantalla
+xWindow =windowInitP.winfo_screenwidth()
+yWindow= windowInitP.winfo_screenheight()
+windowComplit= str(xWindow) + "x" + str(yWindow) + "+0+0"
+windowInitP.geometry("800x600+0+0")
+print(windowComplit)
+
 windowInitP.attributes('-topmost', True,'-fullscreen', True)
-#windowInitP.geometry("800x600+0+0")
 windowInitP.title("Registro Pilares")
 windowInitP.config(bg=c6)
 
-#Imagenes pilares
+
+#Imagenes pilares	
 imageLP = tk.PhotoImage(file="img\Soy_PILARES.png").subsample(5)
 imageLPSmall = tk.Label(windowInitP,image= imageLP,background=c6)
 imageCP = tk.PhotoImage(file="img\Cintillo.png").subsample(2)
 imageCPSmall = tk.Label(windowInitP,image= imageCP,background=c6)
 
 #Creación contenedores
-invoiceP= tk.Label(windowInitP,text="Folio Pilares",font=("Vendana",16),foreground=c3,background=c6)
-themeU= tk.Label(windowInitP,text="Uso de dispositivo",font=("Source_Sands_Pro",12),foreground=c3,background=c6)
+invoiceP= tk.Label(windowInitP,text="Escribe tu folio PILARES",font=("Vendana",16),foreground=c3,background=c6)
 messageTxt =tk.Text(windowInitP,width=30,height=2)
 
-buttonOne = tk.Button(windowInitP,text ="Académico",
+hackP= tk.Frame(windowInitP,bg=c6)
+
+themeU= tk.Label(hackP,text="¿Para qué vas a usar este equipo?",font=("Vendana",12),foreground=c3,background=c6)
+
+buttonOne = tk.Button(hackP,text ="Académico",
 width= 15,height =2,background=c4,
 activeforeground=c6,activebackground=c1,
 foreground=c3,command= lambda: invoicePilares("Academico"))
 
-buttonTwo = tk.Button(windowInitP,text ="Laboral",
+buttonTwo = tk.Button(hackP,text ="Laboral",
 width= 15,height =2,background=c4,
 activeforeground=c6,activebackground=c1,
 foreground=c3,command= lambda: invoicePilares("Laboral"))
 
-buttonThree = tk.Button(windowInitP,text ="Servicios",
+buttonThree = tk.Button(hackP,text ="Servicios",
 width= 15,height =2,background=c4,
 activeforeground=c6,activebackground=c1,
 foreground=c3,command= lambda: invoicePilares("Servicios"))
 
-buttonFour = tk.Button(windowInitP,text ="Lúdico",
+buttonFour = tk.Button(hackP,text ="Lúdico",
 width= 15,height =2,background=c4,
 activeforeground=c6,activebackground=c1,
 foreground=c3,command= lambda: invoicePilares("Ludico"))
 
-buttonFive = tk.Button(windowInitP,text ="Trámite",
+buttonFive = tk.Button(hackP,text ="Trámite",
 width= 15,height =2,background=c4,
 activeforeground=c6,activebackground=c1,
 foreground=c3,command= lambda: invoicePilares("Tramite"))
 
+
+# Expandir horizontalmente a columna 0.
+windowInitP.columnconfigure(0, weight=1)
+# Expandir verticalmente la fila 0.
+windowInitP.rowconfigure(0, weight=1)
+
 #Ubicación contenedores
-imageLPSmall.grid(row=1,column=0,columnspan=8,padx=5,pady =0)
-invoiceP.grid(row=2,column=0,columnspan=5,padx=5,pady =10)
-messageTxt.grid(row=3,column=1,columnspan=3,padx=5,pady =5)
-themeU.grid(row=4,column=1,columnspan=3,padx=5,pady =5)
-buttonOne.grid(row=rowB,column=0,padx=20,pady =10)
-buttonTwo.grid(row=rowB,column=1,padx=20)
-buttonThree.grid(row=rowB,column=2,padx=20)
-buttonFour.grid(row=rowB,column=3,padx=20)
-buttonFive.grid(row=rowB,column=4,padx=20)
-imageCPSmall.grid(row=rowB+1,column=0,columnspan=5,padx=5,pady =5)
+
+imageLPSmall.grid(row=rowB-3,
+column=0,columnspan=5,padx=5,pady =0)
+invoiceP.grid(row=rowB-2,
+column=0,columnspan=5,padx=5,pady =20)
+messageTxt.grid(row=rowB-1,
+column=0,columnspan=5,padx=5,pady =5)
+hackP.grid(row=rowB,
+column=0,columnspan=5,padx=5,pady =100)
+themeU.grid(row=1,
+column=0,columnspan=5,pady =20)
+buttonOne.grid(row=2,
+column=0,padx=20)
+buttonTwo.grid(row=2,
+column=1,padx=20)
+buttonThree.grid(row=2,
+column=2,padx=20)
+buttonFour.grid(row=2,
+column=3,padx=20)
+buttonFive.grid(row=2,
+column=4,padx=20)
+imageCPSmall.grid(row=rowB+1,
+column=0,columnspan=5,padx=5,pady =0)
 
 sTmpFileStamp = time.strftime('%Y%m%d%H%M')
 
