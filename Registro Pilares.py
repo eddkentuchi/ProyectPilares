c9568394db9d94cda7fd79f5a504b49d894f61de
EddKentuchi <ediarpoll@gmail.com>
2022-11-03 13:54:52 -0600

    Esructura para sql & Figuras de PILARES

--- Registro Pilares.py
+++ Registro Pilares.py
@@ -3,19 +3,41 @@ from platform import win32_edition
 import tkinter as tk
 from tkinter import messagebox
 import time
+"""
 from datetime import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
+cred = credentials.Certificate('firebase-sdk.json')
+
+firebase_admin.initialize_app(cred)
+
+db= firestore.client()
+
+doc_ref = db.collection('employee').document('empdoc')
+
+doc_ref.set({
+		'name' : 'Eduardo'
+		'iname'
+
+
+
+})
+"""
 #Creación de archivo
 sFileStamp = time.strftime('%Y%m%d')
 sFileName = '\ProyectPilares\Data\PILARES' + sFileStamp + '.txt'
 f=open(sFileName, 'a')
 
-#Guardado de estado de tiempo
-sTimeStamp = time.strftime('%H%M%S')
+#Guardado de estado de tiempo y variables globales
+sTimeStampInit = time.strftime('%H%M%S') 
+sTimeStampFinish = time.strftime('%H%M%S')
+usePilares = "Sin Uso"
 #f.write(sTimeStamp + '\n')
-print (sTimeStamp)
+#print (sTimeStamp)
 #Variable de fila de botones
-rowB= 4
+rowB= 1
 
 #Colores
 c1= "#9F2241" #Rojo
@@ -33,10 +55,14 @@ if sFileStamp != sTmpFileStamp:
 #Definición de función de folio
 def invoicePilares(themePilares):
 	messageP= messageTxt.get(1.0,"end-1c")
+	global usePilares
+	usePilares =themePilares
+	global sTimeStampInit
+	sTimeStampInit = time.strftime('%H%M%S') 
+
+	#usePilares = themePilares
 	if len(messageP)!=0:
-		f=open(sFileName, 'a')
-		f.write(themePilares + ',' + messageP +','+ sTimeStamp +'\n')
-		print (themePilares + ',' + messageP +','+ sTimeStamp +'\n')
+		#print (themePilares +'\n')
 		openNewW()
 	else:
 		print("vacío")
@@ -50,6 +76,7 @@ def openNewW():
 	win2=tk.Toplevel(windowInitP)
 	win2.attributes('-topmost', True)
 	win2.geometry("120x60+0+0")
+	#win2.iconbitmap("img\Soy_PILARES2.PNG") #Cambiar el icono
 	win2.title("Registro Pilares")
 	win2.config(background=c6)
 	#imageLP2 = tk.PhotoImage(file="Soy_PILARES2.png").subsample(10)
@@ -62,10 +89,14 @@ def openNewW():
 	buttonSix.grid(row=1,column=1,padx=5,pady =10)
 
 #Ventana Inicial
-def initPilares(themePilares):
-	sTimeStamp = time.strftime('%H%M%S')
-	f.write(themePilares + ','+ sTimeStamp +'\n')
-	print (themePilares + ','+ sTimeStamp +'\n')
+def initPilares(finishTime):
+	messagePi= messageTxt.get(1.0,"end-1c")
+	print(finishTime)
+	global sTimeStampFinish
+	sTimeStampFinish = time.strftime('%H%M%S')
+	f=open(sFileName, 'a')
+	f.write(usePilares + ',' + messagePi +','+ sTimeStampInit +','+sTimeStampFinish +'\n')
+	print (usePilares + ',' + messagePi +','+ sTimeStampInit +','+sTimeStampFinish +'\n')
 	f.close()
 	windowInitP.deiconify()
 	win2.withdraw()
@@ -77,7 +108,7 @@ xWindow =windowInitP.winfo_screenwidth()
 yWindow= windowInitP.winfo_screenheight()
 windowComplit= str(xWindow) + "x" + str(yWindow) + "+0+0"
 windowInitP.geometry("800x600+0+0")
-print(windowComplit)
+#print(windowComplit)
 
 windowInitP.attributes('-topmost', True,'-fullscreen', True)
 windowInitP.title("Registro Pilares")
@@ -91,8 +122,11 @@ imageCP = tk.PhotoImage(file="img\Cintillo.png").subsample(2)
 imageCPSmall = tk.Label(windowInitP,image= imageCP,background=c6)
 
 #Creación contenedores
-invoiceP= tk.Label(windowInitP,text="Escribe tu folio PILARES",font=("Vendana",16),foreground=c3,background=c6)
-messageTxt =tk.Text(windowInitP,width=30,height=2)
+sign_in= tk.Frame(windowInitP,bg=c6)
+invoiceP= tk.Label(sign_in,text="Escribe tu folio PILARES",font=("Vendana",16),foreground=c3,background=c6)
+messageTxt =tk.Text(sign_in,width=30,height=2)
+teacherP= tk.Label(sign_in,text="Figura que atiende",font=("Vendana",16),foreground=c3,background=c6)
+idTeacher =tk.Text(sign_in,width=30,height=2)
 
 hackP= tk.Frame(windowInitP,bg=c6)
 
@@ -131,14 +165,22 @@ windowInitP.rowconfigure(0, weight=1)
 
 #Ubicación contenedores
 
-imageLPSmall.grid(row=rowB-3,
+imageLPSmall.grid(row=rowB,
 column=0,columnspan=5,padx=5,pady =0)
-invoiceP.grid(row=rowB-2,
-column=0,columnspan=5,padx=5,pady =20)
-messageTxt.grid(row=rowB-1,
-column=0,columnspan=5,padx=5,pady =5)
-hackP.grid(row=rowB,
-column=0,columnspan=5,padx=5,pady =100)
+
+sign_in.grid(row=rowB+1,
+column=0,columnspan=6,padx=5,pady =50)
+invoiceP.grid(row=1,
+column=0,columnspan=2,padx=5,pady =20)
+messageTxt.grid(row=2,
+column=0,columnspan=2,padx=50,pady =5)
+teacherP.grid(row=1,
+column=4,columnspan=2,padx=5)
+idTeacher.grid(row=2,
+column=4,columnspan=2,padx=50)
+
+hackP.grid(row=rowB+2,
+column=0,columnspan=5,padx=5,pady =50)
 themeU.grid(row=1,
 column=0,columnspan=5,pady =20)
 buttonOne.grid(row=2,
@@ -151,10 +193,8 @@ buttonFour.grid(row=2,
 column=3,padx=20)
 buttonFive.grid(row=2,
 column=4,padx=20)
-imageCPSmall.grid(row=rowB+1,
+imageCPSmall.grid(row=rowB+3,
 column=0,columnspan=5,padx=5,pady =0)
-
-sTmpFileStamp = time.strftime('%Y%m%d%H%M')
 
 
 
